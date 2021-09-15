d4030736785739b3443289cfadc0cf0b31fedfe3
Abid Al Wassie <abidalwassie@outlook.com>
2021-09-15 07:18:45 +0600

    updated project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  const profileBtn = document.getElementById("profile");
+  const profileContainer = document.getElementById("profileContainer");
+
+  profileBtn.addEventListener("click", profileToggle);
+
+  function profileToggle() {
+    profileContainer.classList.toggle("hide");
+  }
+}
